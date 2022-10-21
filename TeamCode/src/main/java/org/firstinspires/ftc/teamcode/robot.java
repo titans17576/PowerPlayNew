dc74d4fa98d8c78304867033666d617eefa8ebd6
Michael Meng <75584886+michaelm0777@users.noreply.github.com>
2022-10-20 21:37:36 -0500

    robot.java-ed +1 int

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.roadrunner.drive.MecanumDrive;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kA;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kStatic;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.TRACK_WIDTH;
+
+
+
+import java.util.List;
+
+public class robot extends MecanumDrive {
+    public static double LATERAL_MULTIPLIER=1.246; // yo no se que es un LateralMultiplier
+
+    public robot(HardwareMap ahwMap){
+        super(kV, kA, kStatic, TRACK_WIDTH, TRACK_WIDTH, LATERAL_MULTIPLIER);
+
+    }
+
+    @Override
+    protected double getRawExternalHeading() {
+        return 0;
+    }
+
+    @NonNull
+    @Override
+    public List<Double> getWheelPositions() {
+        return null;
+    }
+
+    @Override
+    public void setMotorPowers(double v, double v1, double v2, double v3) {
+
+    }
+}
