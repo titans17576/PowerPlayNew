ba9ea556db1ac137bea823a1c035be10510e354d
Sree Atyam <sreeatyam346@gmail.com>
2022-11-12 08:14:27 -0600

    added controls for slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop1.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Teleop1")
@@ -12,42 +13,52 @@ public class Teleop1 extends LinearOpMode {
         robot R = new robot(hardwareMap);
         waitForStart();
         while(opModeIsActive()){
-            /*R.setWeightedDrivePower(
-                    new Pose2d(
-                            -gamepad1.left_stick_x,
-                            -gamepad1.left_stick_y,
-                            gamepad1.right_stick_x
-                    )
-            );
-            R.update();*/
-
-            /*float y1 = -gamepad1.left_stick_y;
-            float x1 = gamepad1.left_stick_x;
-            float yaw = gamepad1.right_stick_x;
-            float shift = 1f;
-
-
-            int reverse;
-            if (gamepad1.right_bumper){
-                reverse = -1;
+            if (gamepad1.dpad_down) {
+                R.slide.setTargetPosition(0);
+                R.slide.setPower(-0.8);
+                R.slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                while(R.slide.isBusy()){
+                    telemetry.addData("Status", "0");
+                    telemetry.update();
+                }
+                R.slide.setPower(0);
             }
-            else {
-                reverse = 1;
-            }
+            if (gamepad1.dpad_left) {
+                R.slide.setTargetPosition(663);
+                R.slide.setPower(0.8);
+                R.slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                while(R.slide.isBusy()){
+                    telemetry.addData("Status", "0");
+                    telemetry.update();
+                }
 
-            double lb = Range.clip((((y1 * reverse) + yaw - (x1 * reverse)) * shift), -1.0, 1.0);
-            double rb = Range.clip((((y1 * reverse) - yaw + (x1 * reverse)) * shift), -1.0, 1.0);
-            double lf = Range.clip((((y1 * reverse) + yaw + (x1 * reverse)) * shift), -1.0, 1.0);
-            double rf = Range.clip((((y1 * reverse) - yaw - (x1 * reverse)) * reverse * shift), -1.0, 1.0);
-            telemetry.addData("lf", lf);
-            telemetry.addData("rf", rf);
-            telemetry.addData("lb", lb);
-            telemetry.addData("rb", rb);
+            }
+            if (gamepad1.dpad_up) {
+                R.slide.setTargetPosition(1362);
+                R.slide.setPower(0.8);
+                R.slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                while(R.slide.isBusy()){
+                    telemetry.addData("Status", "0");
+                    telemetry.update();
+                }
+
+            }
+            if (gamepad1.dpad_right) {
+                R.slide.setTargetPosition(1990);
+                R.slide.setPower(0.8);
+                R.slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                while(R.slide.isBusy()){
+                    telemetry.addData("Status", "0");
+                    telemetry.update();
+                }
+
+            }
+            if (gamepad1.a) {
+
+                R.slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            }
+            telemetry.addData("Ticks", R.slide.getCurrentPosition());
             telemetry.update();
-            R.rightFront.setPower(rf);
-            R.leftFront.setPower(lf);
-            R.leftRear.setPower(lb);
-            R.rightRear.setPower(rb);*/
             double drive = -gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
             double strafe = gamepad1.left_stick_x;
@@ -57,7 +68,6 @@ public class Teleop1 extends LinearOpMode {
             R.rightRear.setPower(calcPower(drive - turn + strafe, slow));
             R.rightFront.setPower(calcPower(drive - turn - strafe, slow));
         }
-
     }
     static double calcPower(double power, double slow){
         return power * power * Math.signum(power) * slow;
